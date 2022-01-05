898a58151b6d7ed47f69b228695eacbea98da65c
windowsyl <ericc@khanlabschool.org>
2022-01-05 09:45:18 -0800

    hello world

---
+++ hello-world-greeter.js
@@ -0,0 +1,4 @@
+console.log("hi my name is sylvan")
+console.log("i have a bit of coding experience, but not much with js")
+console.log("technically i'm submitting this before the class starts so maybe that counts")
+console.log("i'm excited to use apis and stuff")
