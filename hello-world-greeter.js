0326017eb4ba15294a6edb121002d96b104d05ae
windowsyl <ericc@khanlabschool.org>
2022-01-05 10:25:33 -0800

    Update hello-world-greeter.js

--- hello-world-greeter.js
+++ hello-world-greeter.js
@@ -1,4 +1,15 @@
+console.time("\nthis code ran in")
 console.log("hi my name is sylvan")
 console.log("i have a bit of coding experience, but not much with js")
 console.log("technically i'm submitting this before the class starts so maybe that counts")
 console.log("i'm excited to use apis and stuff")
+console.timeEnd("\nthis code ran in")
+
+console.log("\ni'm going to try doing this in one console.log call")
+console.time("\nthis version ran in")
+let msg = "hi my name is sylvan" 
+msg += "\n" + "i have a bit of coding experience, but not much with js" 
+msg += "\n" + "technically i'm submitting this before the class starts so maybe that counts"
+msg += "\n" + "i'm excited to use apis and stuff" 
+console.log(msg)
+console.timeEnd("\nthis version ran in")
